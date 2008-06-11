0054c3579218005da30809e904a8b9d4a0ab585a
Lee Olayvar <leefmcpub+gitpub@gmail.com>
2008-06-11 06:25:39 -0700

    gui.py Tab Layout Changes
    Implemented a bit more of the tab structure, and started the layout for
    the first tab layout, "GUIDE".

--- bmrf/gui.py
+++ bmrf/gui.py
@@ -18,6 +18,9 @@ class MainGUI(object):
     EVT_GUIDE_TAB = 1100
     EVT_RIG_TAB = 1200
     EVT_XML_TAB = 1300
+    EVT_MODULES_TAB = 1400
+    
+    EVT_GUIDETAB_GNAME = 1101
     
     # Generic Dimensions.
     ## Keep in mind changes to these may impact more than you'd expect,
@@ -49,7 +52,42 @@ class MainGUI(object):
         Blender seems to initialize the drawing function (L{self._gui}) many
         times in one session, possibly whenever blender/this gui loses focus.
         '''
-        pass
+        # Tab Name
+        Blender.Draw.Label(
+            'TAB: GUIDE',
+            self.gui_border_locations['left'], # X, y, w, h)
+            self.gui_border_locations['top'] - 20, # Y - LabelHeight
+            self.gui_width, # Width
+            20 # Height
+        )
+        
+        # Guide Name
+        Blender.Draw.String(
+            'Guide Name: ', self.EVT_GUIDETAB_GNAME,
+            self.gui_border_locations['left'], # X
+            self.gui_border_locations['top'] - 40, # Y - Height+TabName
+            self.gui_width*0.75, 20, # Width, Height
+            'bmrf_guide', # Default
+            25, # Max Char Length
+            'Test' # Tooltip
+        )
+    
+    def _draw_modules_tab_gui(self):
+        '''Draw the GUI for the module tab's contents.
+        
+        @attention: Make sure only drawing code is within the drawing functions.
+        Blender seems to initialize the drawing function (L{self._gui}) many
+        times in one session, possibly whenever blender/this gui loses focus.
+        '''
+        
+        # Tab Name
+        Blender.Draw.Label(
+            'TAB: MODULES',
+            self.gui_border_locations['left'], # X, y, w, h)
+            self.gui_border_locations['top'] - 20, # Y - LabelHeight
+            self.gui_width, # Width
+            20 # Height
+        )
     
     def _draw_rig_tab_gui(self):
         '''Draw the GUI for the rig tab's contents.
@@ -58,7 +96,15 @@ class MainGUI(object):
         Blender seems to initialize the drawing function (L{self._gui}) many
         times in one session, possibly whenever blender/this gui loses focus.
         '''
-        pass
+        
+        # Tab Name
+        Blender.Draw.Label(
+            'TAB: RIGGING',
+            self.gui_border_locations['left'], # X, y, w, h)
+            self.gui_border_locations['top'] - 20, # Y - LabelHeight
+            self.gui_width, # Width
+            20 # Height
+        )
     
     def _draw_stateless_gui(self):
         '''Draw the stateless gui. ie, the one which is on every tab state.
@@ -84,20 +130,25 @@ class MainGUI(object):
             button_width * 2 + self.gui_border_locations['left'],
             self.gui_border_locations['bottom'], button_width, button_height)
         
-        tab_width = self.gui_width/3
+        tab_width = self.gui_width/4
         Blender.Draw.PushButton(
             'GUIDE', self.EVT_GUIDE_TAB,
             self.gui_border_locations['left'] + (tab_width*0),
             20 + self.gui_border_locations['bottom'],
             tab_width, button_height)
         Blender.Draw.PushButton(
-            'RIG', self.EVT_RIG_TAB,
+            'MODULES', self.EVT_MODULES_TAB,
             self.gui_border_locations['left'] + (tab_width*1),
             20 + self.gui_border_locations['bottom'],
             tab_width, button_height)
         Blender.Draw.PushButton(
-            'XML', self.EVT_XML_TAB,
+            'RIG', self.EVT_RIG_TAB,
             self.gui_border_locations['left'] + (tab_width*2),
+            20 + self.gui_border_locations['bottom'],
+            tab_width, button_height)
+        Blender.Draw.PushButton(
+            'XML', self.EVT_XML_TAB,
+            self.gui_border_locations['left'] + (tab_width*3),
             20 + self.gui_border_locations['bottom'],
             tab_width, button_height)
     
@@ -108,12 +159,21 @@ class MainGUI(object):
         Blender seems to initialize the drawing function (L{self._gui}) many
         times in one session, possibly whenever blender/this gui loses focus.
         '''
-        pass
+        
+        # Tab Name
+        Blender.Draw.Label(
+            'TAB: XML',
+            self.gui_border_locations['left'], # X, y, w, h)
+            self.gui_border_locations['top'] - 20, # Y - LabelHeight
+            self.gui_width, # Width
+            20 # Height
+        )
     
     ## Defined after the _draw's so it can reference them.
     # Draw Tab Switch, responsible for calling the draw functions for each tab.
     draw_tab_switch = {
         'guide':_draw_guide_tab_gui,
+        'modules':_draw_modules_tab_gui,
         'rig':_draw_rig_tab_gui,
         'xml':_draw_xml_tab_gui,
     }
@@ -128,13 +188,17 @@ class MainGUI(object):
         
         # Define the python switch for the event ID
         event_switch = {
+            # Bottom Buttons
             self.EVT_CLOSEGUI_BTN:self.closegui_btn_clicked_event,
             self.EVT_ABOUT_BTN:self.about_btn_clicked_event,
             self.EVT_HELP_BTN:not_handled,
-            
+            # Tab Buttons
             self.EVT_GUIDE_TAB:self.guide_tab_clicked_event,
+            self.EVT_MODULES_TAB:self.modules_tab_clicked_event,
             self.EVT_RIG_TAB:self.rig_tab_clicked_event,
             self.EVT_XML_TAB:self.xml_tab_clicked_event,
+            # Guide Tab Events
+            self.EVT_GUIDETAB_GNAME:not_handled,
         }
         # Call the switch
         event_switch[event_id]()
@@ -190,6 +254,13 @@ class MainGUI(object):
         '''
         if self.current_tab != 'guide':
             self.current_tab = 'guide'
+            self.redraw_display()
+    
+    def modules_tab_clicked_event(self):
+        '''Display the Modules Tab.
+        '''
+        if self.current_tab != 'modules':
+            self.current_tab = 'modules'
             self.redraw_display()
     
     def redraw_display(self):
