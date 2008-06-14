136e29770e176a69b138ce43086bafc529c175b0
Lee Olayvar <leefmcpub+gitpub@gmail.com>
2008-06-14 12:11:23 -0700

    Added missing __init__s.

---
+++ bcrf/bcrf_lib/guides/__init__.py
@@ -0,0 +1,7 @@
+'''This package contains code specific to the full Guides, not the specific
+component guides.'''
+
+# standard
+# related
+# local
+
---
+++ bcrf/bcrf_lib/rigs/__init__.py
@@ -0,0 +1,7 @@
+'''This package contains code specific to the entire rig, such as the code which
+creates the rig, rigs all of its components, etc.'''
+
+# standard
+# related
+# local
+
